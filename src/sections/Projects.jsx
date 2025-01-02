9f829d8d0098594fdaa262728cc6b79014586bf0
Suyash <tiwarisuyash40@gmail.com>
2025-01-02 14:35:46 +0530

    adjusted the about and project section styles

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -33,8 +33,8 @@ const Projects = () => {
     <section className="c-space my-20">
       <p className="head-text">My Selected Work</p>
 
-      <div className="grid lg:grid-cols-2 grid-cols-1 mt-12 gap-5 w-full">
-        <div className="flex flex-col gap-5 relative sm:p-10 py-10 px-5 shadow-2xl shadow-black-200">
+      <div className="grid lg:grid-cols-2 grid-cols-1 mt-12 gap-5 w-full ">
+        <div className="flex flex-col gap-5 relative sm:p-10 py-10 px-5 shadow-2xl shadow-black-200 order-2 sm:order-1">
           <div className="absolute top-0 right-0">
             <img src={currentProject.spotlight} alt="spotlight" className="w-full h-96 object-cover rounded-xl" />
           </div>
@@ -80,7 +80,7 @@ const Projects = () => {
           </div>
         </div>
 
-        <div className="border border-black-300 bg-black-200 rounded-lg h-96 md:h-full">
+        <div className="border border-black-300 bg-black-200 rounded-lg h-96 md:h-full order-1">
           <Canvas>
             <ambientLight intensity={Math.PI} />
             <directionalLight position={[10, 10, 5]} />
